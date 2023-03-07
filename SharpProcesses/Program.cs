23b6dbd1735fecd2aacdf0e6d8b1b369251978c1
Diogo Miguel da Silva Mendes <72610397+DiogoFreeDom@users.noreply.github.com>
2023-03-07 22:06:44 +0000

    Full functionality
    Program works as intended, however I couldn't implement the unit tests...

---
+++ SharpProcesses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace SharpProcesses
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            String targetProcess = args[0];
+            Double maxLife = Double.Parse(args[1]); // in minutes
+            Double monitorFreq = Double.Parse(args[2]) * 60 * 1000; // received in minutes, transformed to miliseconds
+
+            Timer scheduler = new Timer(monitorFreq);
+            scheduler.AutoReset = true;
+            scheduler.Elapsed += (sender, e) => monitorProcess(sender, e, targetProcess, maxLife);
+            scheduler.Enabled = true;
+
+            Console.WriteLine("\nMonitoring processes. Start time: " + DateTime.Now.ToString());
+            Console.WriteLine("Press Enter to stop monitoring...");
+            Console.ReadLine();
+
+            scheduler.Stop();
+            scheduler.Dispose();
+        }
+
+        static void monitorProcess(Object source, ElapsedEventArgs e, String targetProcess, Double maxLife)
+        {
+            Console.WriteLine("Monitoring event triggered at " + DateTime.Now.ToString());
+
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName == targetProcess && (DateTime.Now - process.StartTime).TotalMinutes > maxLife)
+                {
+                    Console.WriteLine(process.ProcessName + " lasted " + (DateTime.Now - process.StartTime).TotalMinutes + "minutes, process was killed.");
+                    process.Kill();
+                }
+            }
+        }
+    }
+}
